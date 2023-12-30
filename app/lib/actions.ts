df51fcbd4637c7cb78099a79cf0891577fe616aa
Yuto Yama <yuvto.0420@gmail.com>
2023-12-29 16:48:30 -0800

    Create a schema for form data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,25 @@
-'use server';
+"use server";
+
+import { z } from "zod";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["paid", "pending"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    }
-
-    console.log(rawFormData);
+  // Type validation
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  // It's usually good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
 }

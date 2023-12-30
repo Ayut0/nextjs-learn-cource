32b1194e6de376320b00b119a59673a066257574
Yuto Yama <yuvto.0420@gmail.com>
2023-12-29 16:55:15 -0800

    Add SQL query to insert the invoice data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { z } from "zod";
+import { sql } from "@vercel/postgres";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -22,4 +23,9 @@ export async function createInvoice(formData: FormData) {
   // It's usually good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
 }

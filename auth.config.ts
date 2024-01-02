c7315a418dbfc31be9629451583e2f2496ec3c33
Yuto Yama <yuvto.0420@gmail.com>
2024-01-01 18:18:36 -0800

    Add a middleware to check credentials

--- auth.config.ts
+++ auth.config.ts
@@ -1,11 +1,26 @@
 import type { NextAuthConfig } from "next-auth";
 
-// This is not required, but by adding signIn: '/login' into our pages option, 
-// the user will be redirected to our custom login page, 
+// This is not required, but by adding signIn: '/login' into our pages option,
+// the user will be redirected to our custom login page,
 // rather than the NextAuth.js default page.
 
 export const authConfig = {
   pages: {
     signIn: "/login",
   },
-};
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return false;
+    },
+    },
+  providers: []
+} satisfies NextAuthConfig;
